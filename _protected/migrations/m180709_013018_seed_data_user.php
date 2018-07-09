cb619bf0acd93cfad4638582f899332a86f9e6c9
Heru Arief Wijaya <hoaaah.arief@gmail.com>
2018-07-09 08:35:22 +0700

    Seed Data User

---
+++ _protected/migrations/m180709_013018_seed_data_user.php
@@ -0,0 +1,52 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m180709_013018_seed_data_user
+ */
+class m180709_013018_seed_data_user extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->insert('{{%user}}', [
+            'id' => 1,
+            'username' => 'administrator',
+            'email' => 'admin@admin.com',
+            'password_hash' => Yii::$app->getSecurity()->generatePasswordHash('administrator'),
+            'auth_key' => Yii::$app->getSecurity()->generateRandomString(),
+            'status' => User::STATUS_ACTIVE,
+            'created_at' => time(),
+            'updated_at' => time()
+        ]);
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m180709_013018_seed_data_user cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m180709_013018_seed_data_user cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
